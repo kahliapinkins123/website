e198e22c031f276e2bd3a3361a1043afc6adff27
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-08-23 20:08:11 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "Welcome to the party! I knew you'd be arriving soon!";
+
+document.querySelector("body").appendChild(p);
